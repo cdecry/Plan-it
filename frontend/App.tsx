44e230952b977858f1ff0203d5c018a65c1b1c91
crystal huang <35664551+cdecry@users.noreply.github.com>
2022-11-05 18:36:41 -0400

    Setup push notifications, trigger from within app (#3)

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -4,19 +4,58 @@ import { SafeAreaProvider } from 'react-native-safe-area-context';
 import useCachedResources from './hooks/useCachedResources';
 import useColorScheme from './hooks/useColorScheme';
 import Navigation from './navigation';
+import * as Notifications from 'expo-notifications';
+import { useEffect } from 'react';
+
+Notifications.setNotificationHandler({
+    handleNotification: async () => ({
+      shouldShowAlert: true,
+      shouldPlaySound: false,
+      shouldSetBadge: false,
+    }),
+  });  
+
+
+  declare global {
+    var token: string;
+  }
 
 export default function App() {
-  const isLoadingComplete = useCachedResources();
-  const colorScheme = useColorScheme();
+    useEffect(() => {
+        registerForPushNotificationsAsync().catch(error => console.log('error: ', error));;
+    }, []);
+    async function registerForPushNotificationsAsync() {
+        let token;
+    
+        const { status: existingStatus } = await Notifications.getPermissionsAsync();
+        let finalStatus = existingStatus;
+    
+        if (existingStatus !== 'granted') {
+            const { status } = await Notifications.requestPermissionsAsync();
+            finalStatus = status;
+        }
+        if (finalStatus !== 'granted') {
+            alert('Failed to get push token for push notification!');
+            return;
+        }
+        token = (await Notifications.getExpoPushTokenAsync()).data;
+        console.log(token);
+        global.token = token;
+        // console.log(global.token);
+        return token;
+    }
 
-  if (!isLoadingComplete) {
+    const isLoadingComplete = useCachedResources();
+    const colorScheme = useColorScheme();
+
+    if (!isLoadingComplete) {
     return null;
-  } else {
+    } else {
     return (
-      <SafeAreaProvider>
+        <SafeAreaProvider>
         <Navigation colorScheme={colorScheme} />
         <StatusBar />
-      </SafeAreaProvider>
+        </SafeAreaProvider>
     );
-  }
+    }
 }
