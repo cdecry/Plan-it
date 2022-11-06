59553e5cefb8cc6fd97fb5ec9cf10f778044fc14
Tyler3412 <62577262+Tyler3412@users.noreply.github.com>
2022-11-06 09:11:04 -0500

    Added settings page

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -46,7 +46,7 @@ export default function App() {
     }
 
     const isLoadingComplete = useCachedResources();
-    const colorScheme = useColorScheme();
+    const colorScheme = 'dark';
 
     if (!isLoadingComplete) {
     return null;
